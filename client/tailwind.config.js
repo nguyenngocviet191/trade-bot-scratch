4e7509491757ff34f0561d244cfcc9e5e630b4c4
Nguyễn Ngọc Việt <nguyenngocviet.mta@gmail.com>
2025-02-28 16:08:06 +0700

    Add Tailwind CSS configuration and update dependencies

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}"
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./web_test.html"
     ],
     theme: {
       extend: {},
